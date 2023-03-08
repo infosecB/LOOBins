3b9c4ad1d67b5df6958152d9f4a53c378f80c968
infosecB <brendan@infosecb.com>
2023-03-08 08:24:21 -0500

    Change: cleaned up code

--- py_loobins/models.py
+++ py_loobins/models.py
@@ -1,29 +1,45 @@
-from pydantic import BaseModel
+"""Models that represent a LOOBin and its various components"""
 from typing import List, Optional, Union
 from datetime import datetime
+from pydantic import BaseModel
+
 
 class Detection(BaseModel):
+    """Detection model"""
+
     source: str
     url: Union[List[str], str]
 
+
 class ExternalReference(BaseModel):
+    """External reference model"""
+
     name: str
     url: str
 
-class Path(BaseModel):
+
+class FullPath(BaseModel):
+    """Full path model"""
+
     path: str
 
+
 class Command(BaseModel):
+    """Command model"""
+
     command: str
     description: str
     use_case: str
 
+
 class LOOBin(BaseModel):
+    """LOOBin model"""
+
     name: str
     description: str
     author: str
-    created: datetime.date
+    created: datetime
     commands: List[Command]
-    full_path: List[Path]
+    full_path: List[FullPath]
     detections: List[Detection]
     external_references: Optional[List[ExternalReference]]
