d9510d8baf815f5a592bf12a62656c0aac4ad3b3
infosecB <brendan@infosecb.com>
2023-03-13 20:39:39 -0400

    Added create-template command

--- src/pyloobins/cli.py
+++ src/pyloobins/cli.py
@@ -1,25 +1,38 @@
-'''Model for CLI functions'''
+"""Model for CLI functions"""
 import sys
 import click
-from .util import validate_loobin
+import yaml
+from .util import validate_loobin, make_template
+
 
 @click.group()
 def cli():
-    '''Create, manage, and validate LOOBin objects.'''
+    """Create, manage, and validate LOOBin objects."""
+
 
 @cli.command()
-@click.option('--file', type=str, required=True, help='File location of the LOOBin YAML file to validate.')
-def validate(file: str)->None:
-    '''Validate a LOOBin object.'''
+@click.option(
+    "--file",
+    type=str,
+    required=True,
+    help="File location of the LOOBin YAML file to validate.",
+)
+def validate(file: str) -> None:
+    """Validate a LOOBin object."""
     if validate_loobin(yml_path=file):
         print(f"LOOBin at {file} is valid.")
     else:
         print(f"LOOBin at {file} is NOT valid.")
 
-@cli.command()
-def create():
-    '''Create a new LOOBin object.'''
-    click.echo('You ran the create command.')
 
-if __name__=="__main__":
+@cli.command()
+def create_template():
+    """Create a new LOOBin template file."""
+    template = make_template()
+    with open(file="template.yml", mode="w", encoding="utf-8") as f:
+        f.write(yaml.dump(template.dict(), Dumper=yaml.Dumper))
+        f.close()
+
+
+if __name__ == "__main__":
     sys.exit(cli())
