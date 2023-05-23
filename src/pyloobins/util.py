0a6b8d830a898a7ef97c3ba1434a16ec382c76ee
Brendan Chamberlain <infosecB@users.noreply.github.com>
2023-05-22 22:40:10 -0400

    Fix PyLOOBins LOOBins site dir path definition
    - Fixes #124

--- src/pyloobins/util.py
+++ src/pyloobins/util.py
@@ -1,10 +1,10 @@
 """Utility functions that support the CLI and library"""
 import pathlib
-import site
 from datetime import date
 
 import yaml
 
+import pyloobins
 from .models import Detection, ExampleUseCase, LOOBin, Resource
 
 
@@ -14,7 +14,7 @@ def get_loobins(path: str = "") -> list:
     if path:
         yml_files = pathlib.Path(path).glob("**/*.yml")
     else:
-        yml_files = (pathlib.Path(site.getsitepackages()[0]) / "LOOBins").glob(
+        yml_files = (pathlib.Path(pyloobins.__file__).parents[1]/"LOOBins").glob(
             "**/*.yml"
         )
     for yml_file in yml_files:
