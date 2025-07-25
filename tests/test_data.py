83f0bd76917bd7f675b4b367ebccab235a2d1457
Brendan Chamberlain <infosecB@users.noreply.github.com>
2025-07-24 20:28:45 -0400

    Fix CLI create path handling and clean test factory

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,18 @@
 from pyloobins.models import Detection, ExampleUseCase, LOOBin, LOOBinsGroup, Resource
 from polyfactory.factories.pydantic_factory import ModelFactory
 
+
 class DetectionFactory(ModelFactory[Detection]):
     __model__ = Detection
+
 
 class ExampleUseCaseFactory(ModelFactory[ExampleUseCase]):
     __model__ = ExampleUseCase
 
+
 class ResourceFactory(ModelFactory[Resource]):
     __model__ = Resource
+
 
 class LOOBinFactory(ModelFactory[LOOBin]):
     __model__ = LOOBin
